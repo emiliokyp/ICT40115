ddcef7a3ca4216041a7934326757b32271e4fd03
Emilio <emilio.kyp@gmail.com>
2018-08-02 12:37:02 +1000

    init

---
+++ PRG/Week1a/Week1a/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Week1a
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			int variable = 0;
+			int anotherVariable = 100;
+
+			Console.WriteLine ("My first variable is equal to " + variable);
+			Console.WriteLine("My second variable is equal to " + anotherVariable);
+
+			string name = "Emilio";
+			string animal = "dog";
+
+			Console.WriteLine (name + "'s favourite animal is a " + animal);
+
+			int a = 3;
+			int b = 10;
+			int c = a + b;
+
+			Console.WriteLine("3 + 10 = 13");
+			Console.WriteLine(a + " + " + b + " = " + c);
+
+			string colour = "green";
+			string car = "Mazda";
+			string location = "Byron Bay";
+			int streetNumber = 3;
+
+			Console.WriteLine("I just moved here from " + location + " and I left my " + colour + " " + car + " behind.\nIts at " + streetNumber + " Tulipwood court");
+
+
+			// byte
+			// string
+			// float
+			// string
+			// int
+			// short / int
+			// string
+			// string
+			// char
+			// bool
+			// string
+			// string
+
+
+
+
+		}
+	}
+}
---
+++ PRG/Week1b/Week1b/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Week1b
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int a = 1;
+			int b = 5;
+			int c = -30;
+
+			a = a + 1;
+			a += 1;
+
+			Console.WriteLine (a);
+
+			a -= 1;
+
+			a *= 4;
+
+			a /= 2;
+
+			Console.WriteLine (a);
+
+			a = 10;
+			int remainder = a % 3;
+			Console.WriteLine (remainder);
+
+			int days = 700;
+			int years = days / 365;
+			int daysLeftover = days % 365;
+
+			Console.WriteLine ("in " + days + " days there is " + years + " year/s and " + daysLeftover + " days remainder");
+
+			a = 4;
+			b = 10;
+			c = -3;
+
+			// 40
+			// -8
+			// 77
+			// -26
+
+			int total = a * b;
+			Console.WriteLine (total);
+			total = a + a * c;
+			Console.WriteLine (total);
+			total = (b + b) * a + c;
+			Console.WriteLine (total);
+			total = a + a - a + c * b;
+			Console.WriteLine (total);
+				
+			int data = 0;
+			data++; // This is postfix, the increase happens at the end of use;
+			Console.WriteLine (data);
+			++data; // This is prefix, the increase happens before use;
+			Console.WriteLine (data);
+			data++;
+			++data;
+			Console.WriteLine (++data);
+			Console.WriteLine (data++);
+			Console.WriteLine (data);
+
+
+			// 5
+			// 12
+			// 6
+			// 10
+
+			total = ++a;
+			Console.WriteLine (total);
+			total = a++ + ++a;
+			Console.WriteLine (total);
+			total = b + --c;
+			Console.WriteLine (total);
+			total = a + b++ - c + b;
+			Console.WriteLine (total);
+		}
+	}
+}
